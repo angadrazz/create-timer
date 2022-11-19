7d1b457eaffc5b9e5e66f72d0978dc9db2a93231
ANGADMADHEPURA <ANGADRAJ9534@GMAIL.COM>
2022-11-19 23:23:46 +0530

    first commit

---
+++ timer.js
@@ -0,0 +1,13 @@
+function timer() {
+    for(let i = 1; i<=5; i++){
+        setTimeout(function() {
+           //Write your code here...
+            console.log(i);
+          
+        }, //write your code here...);
+                   i*1000);
+    }
+    console.log("Hello ninjas!");
+}
+timer();
+    
